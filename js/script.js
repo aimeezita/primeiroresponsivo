0a5089fd377b6be709e280f0ea17ce57cf1b8026
aimeezita <rosettethompson@gmail.com>
2019-04-12 09:32:33 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("projetos").addEventListener("click", function(){
+    document.getElementById("meusprojetos").innerHTML = window.location.href = '#meusprojetos';
+  });
+
+  
